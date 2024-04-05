5b07f03c3214490714901fe3d180353bf798d7c7
Lars L R <lars.lahlum.ruud@tietoevry.com>
2024-04-05 04:10:57 +0200

    Unify shared cpp/glsl structs into single file. Add support for ray queries. Remove orphaned structs and functions in compute shader.

--- resources/shaders/compile.bat
+++ resources/shaders/compile.bat
@@ -1,4 +1,4 @@
-C:/VulkanSDK/1.3.275.0/Bin/glslc.exe shader.vert -o vert.spv
-C:/VulkanSDK/1.3.275.0/Bin/glslc.exe shader.frag -o frag.spv
-C:/VulkanSDK/1.3.275.0/Bin/glslc.exe shader.comp -o comp.spv
+C:/VulkanSDK/1.3.275.0/Bin/glslc.exe shader.vert --target-env=vulkan1.3 -o vert.spv
+C:/VulkanSDK/1.3.275.0/Bin/glslc.exe shader.frag --target-env=vulkan1.3 -o frag.spv
+C:/VulkanSDK/1.3.275.0/Bin/glslc.exe shader.comp --target-env=vulkan1.3 -o comp.spv
 pause
